aaa0f0594f50d6022b3479565e6a0f5dae406a80
Thomas Clausen <kontakt@thomasclausen.dk>
2013-03-12 09:54:32 +0100

    Change type on input for searchform

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 		<form method="get" id="searchform" class="clearfix" action="<?php echo home_url( '/' ); ?>">
 			<fieldset class="clearfix">
 				<label for="s"><?php _e( 'S&oslash;g', 'bike' ); ?></label>
-				<input type="text" name="s" id="s" placeholder="<?php esc_attr_e( __( 'S&oslash;g', 'bike' ) ); ?>" value="<?php the_search_query(); ?>" />
+				<input type="search" name="s" id="s" placeholder="<?php esc_attr_e( __( 'S&oslash;g', 'bike' ) ); ?>" value="<?php the_search_query(); ?>" />
 			</fieldset>
 			<fieldset class="button clearfix">
 				<input type="submit" class="button" value="<?php esc_attr_e( __( 'S&oslash;g', 'bike' ) ); ?>" />
